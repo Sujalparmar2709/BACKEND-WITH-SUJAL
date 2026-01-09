c096feedd8c50a4b9e29714242a1cf1d66c63c73
Sujalparmar2709 <sujalparmar2709@gmail.com>
2026-01-09 20:57:27 +0530

    how we can make CRUD

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ const app = express();
 
 const userModel = require(`./usermodel`)
 
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
     res.send("hey");
 })
 
-app.get('/create', async (req, res) =>{
+app.get('/create', async (req, res) => {
     let createduser = await userModel.create({
         name: "Sujal",
         username: "sujalparmar",
         email: "sujalparmar2709@gmail.com"
     })
     res.send(createduser);
+})
+
+app.get("/read", async (req, res) => {
+    let users = await userModel.find();
+    res.send(users);
+})
+
+app.get('/update', async (req, res) => {
+    let updateduser = await userModel.findOneAndUpdate({ username: "sujal" }, { name: "sujal parmar" }, { new: true })
+    res.send(updateduser);
+})
+
+
+app.get("/delete", async (req, res) => {
+    let users = await userModel.findOneAndDelete({ username: "sujalparmar" });
+    res.send(users);
 })
 
 app.listen(3000);
